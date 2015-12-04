7c71c509d34923d4deb3b45aa9c3472b375b6db9
Hernan Asorey <asoreyh@gmail.com>
2015-12-04 15:12:28 -0300

    Cleaning dependences solved

--- brc/lago/makestuff/libs/libbuffer/linux.x86_64/rel/.build/conv.o.d
+++
@@ -1,6 +0,0 @@
-linux.x86_64/rel/.build/conv.o: conv.c conv.h \
- /home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/common/makestuff.h
-
-conv.h:
-
-/home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/common/makestuff.h:
--- brc/lago/makestuff/libs/libfx2loader/linux.x86_64/rel/.build/misc.o.d
+++
@@ -1,7 +0,0 @@
-linux.x86_64/rel/.build/misc.o: misc.c \
- /home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/libs/liberror/liberror.h \
- /home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/common/makestuff.h
-
-/home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/libs/liberror/liberror.h:
-
-/home/asoreyh/Dropbox/projects/lago/lago-software/acqua/brc/lago/makestuff/common/makestuff.h:
